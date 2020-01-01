1beb5bfbc7370a656552d1807f44d4aef3a992d4
Jonasz Marek Łaba <jonaszlaba@gmail.com>
2020-01-01 21:26:58 +0100

    almost final

--- GUI.py
+++ GUI.py
@@ -7,11 +7,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 root = tk.Tk()
-root.geometry("600x600")
+root.geometry("1000x600")
+
 def read_entry():
     print(e1.get())
-    
-def open():
+signal = pd.read_csv('syg1.csv')  
+input_feature= signal.iloc[:,[2,4]].values
+script.show_signal(input_feature)  
+def opens():
     global path
     global signal
     global input_feature
@@ -19,7 +22,7 @@ def open():
         root.filename = filedialog.askopenfilename(initialdir  = "/",title="asds")
         path = root.filename
         signal = pd.read_csv(path)
-        input_feature= signal.iloc[:,[2,5]].values
+        input_feature= signal.iloc[:,[2,4]].values
         script.show_signal(input_feature)
         
     except:
@@ -28,14 +31,16 @@ def open():
 
 def sample():
     try:
+        global l
         t = int(e2.get())
-        l = int(e1.get())
+        l = int(((int(e1.get()))/3)*3)
         global s
         global input_x
         global input_y
         s = script.pick_sample(t,l,input_feature)
         script.show_signal(s)
-        input_x, input_y = script.prep_sample(s,250,l)
+        print(l)
+        input_x, input_y = script.prep_sample(s,int(l/3),l)
         
         print("ready to analyze")
     except:
@@ -47,6 +52,8 @@ def analyse():
     global prediction_cnn
     global pred
     global inpt
+    
+    
     if (var2.get() == 'Signal X'):
         inpt = input_x
     elif (var2.get() == 'Signal Y'):
@@ -54,17 +61,32 @@ def analyse():
     
 #    try:
     if (method.get() =='CNN'):
-        model, history = script.create_model_cnn(inpt[0],inpt[1])
+        model, history = script.create_model_cnn(inpt[0],inpt[1],activation.get(),optimizer.get(),loss.get(),int(e4.get()),int(l/3))
         prediction_cnn = model.predict(inpt[2])
         print(prediction_cnn)
         pred = np.transpose(prediction_cnn)
-        fig2 = plt.figure()
+        fig3 = plt.figure()
         plt.plot(pred)
-        fig2.show()
+        fig3.show()
+        
+        
+    elif(method.get() == 'LSTM'):
+        model,history = script.create_model_lstm(inpt[0],inpt[1],activation.get(),optimizer.get(),loss.get(),int(e3.get()),int(l/3))
+        prediction_lstm = model.predict(inpt[2])
+        print(prediction_lstm)
+        pred = np.transpose(prediction_lstm)
+        fig4 = plt.figure()
+        plt.plot(pred)
+        fig4.show()
+        
     elif(method.get() == 'GRU'):
-        model,history = script.create_model_cnn(inpt[0])
-#        prediction_cnn = np.transpose(prediction_cnn)
-#        LSTM_RNN2.show(prediction_cnn)
+        model,history = script.create_model_lstm(inpt[0],inpt[1],activation.get(),optimizer.get(),loss.get(),int(e3.get()),int(l/3))
+        prediction_gru = model.predict(inpt[2])
+        print(prediction_gru)
+        pred = np.transpose(prediction_gru)
+        fig5 = plt.figure()
+        plt.plot(pred)
+        fig5.show()
     else:
         print("blad")
 #    except:
@@ -75,10 +97,17 @@ def compare():
     if (var3.get() == 'MSE'):
         
         print(script.compare(np.transpose(inpt[3]),pred))
+        x = script.compare(np.transpose(inpt[3]),pred)
+        tk.Label(root,text=x).grid(row=7, column = 4)
+        e4 = tk.Entry(root)
+        e4.grid(row=3,column = 5)
+        
+        #Tworzenie pliku raportu analizy
+        
 
     
     
-B = tk.Button(root,text='Open Signal',command = open)
+B = tk.Button(root,text='Open Signal',command = opens)
 B.grid(row=0,column = 0)
 
 tk.Label(root,text="Sample length").grid(row=1, column = 0)
@@ -89,9 +118,15 @@ tk.Label(root,text="Time T0").grid(row=2, column = 0)
 e2 = tk.Entry(root)
 e2.grid(row=2,column = 1)
 
-tk.Label(root,text="Activation function").grid(row=3, column = 0)
+
+
+tk.Label(root,text="Number of LSTM units").grid(row=3, column = 2)
 e3 = tk.Entry(root)
-e3.grid(row=3,column = 1)
+e3.grid(row=3,column = 3)
+
+tk.Label(root,text="CNN kernel size").grid(row=3, column = 4)
+e4 = tk.Entry(root)
+e4.grid(row=3,column = 5)
 
 
 
@@ -99,32 +134,58 @@ e3.grid(row=3,column = 1)
 
 B1 = tk.Button(root,text="show sample",command = sample).grid(row =2, column= 2)
 
-tk.Label(root,text="Choose method").grid(row = 4, column = 0)
+
+
+
+
+tk.Label(root,text="Choose optimizer").grid(row = 4, column = 0)
+optimizers = ['adam','adamax','nadam']
+optimizer = tk.StringVar()
+drop = tk.OptionMenu(root,optimizer,*optimizers)
+drop.grid(row = 4,column = 1)
+
+tk.Label(root,text="Choose loss function").grid(row = 4, column = 2)
+losses = ['mse','mean_absolute_error','squared_hinge']
+loss = tk.StringVar()
+drop = tk.OptionMenu(root,loss,*losses)
+drop.grid(row = 4,column = 3)
+
+tk.Label(root,text="Choose activation").grid(row = 4, column = 4)
+activations = ['relu','elu','tanh']
+activation = tk.StringVar()
+drop = tk.OptionMenu(root,activation,*activations)
+drop.grid(row = 4,column = 5)
+
+tk.Label(root,text="Choose method").grid(row = 5, column = 0)
 methods = ['CNN','LSTM','GRU']
 method = tk.StringVar()
 drop = tk.OptionMenu(root,method,*methods)
-drop.grid(row = 4,column = 1)
+drop.grid(row = 5,column = 1)
 
-tk.Label(root,text="Choose signal").grid(row = 4, column = 2)
+tk.Label(root,text="Choose signal").grid(row = 5, column = 2)
 signals = ['Signal X','Signal Y']
 var2 = tk.StringVar()
 drop2 = tk.OptionMenu(root,var2,*signals)
-drop2.grid(row = 4,column = 3)
+drop2.grid(row = 5,column = 3)
 
 B2 = tk.Button(root,text='Analyse',command = analyse)
-B2.grid(row=4,column = 4)
+B2.grid(row=5,column = 4)
 
 
-tk.Label(root,text="Choose evaluation method").grid(row = 5, column = 0)
+tk.Label(root,text="Choose evaluation method").grid(row = 6, column = 0)
 methods2 = ['MSE','MSA']
 var3 = tk.StringVar()
 drop3 = tk.OptionMenu(root,var3,*methods2)
-drop3.grid(row = 5,column = 0)
+drop3.grid(row = 6,column = 2)
 
 B3 = tk.Button(root,text='Compare',command = compare)
-B3.grid(row=5,column = 1)
-root.mainloop()
+B3.grid(row=6,column = 1)
 
+
+
+
+root.mainloop()
+#
 
 
 
--- script.py
+++ script.py
@@ -61,20 +61,20 @@ def prep_sample(sample,sLength,l):
     train_data = np.array(train_data)
     train_data= train_data.transpose()
     
-    train_data = train_data.reshape(1,250,3)
+    train_data = train_data.reshape(1,sLength,3)
     train_data_y = df.iloc[:,1].values #X2
     train_data_y = np.array(train_data_y)
-    train_data_y = train_data_y.reshape(1,250)
+    train_data_y = train_data_y.reshape(1,sLength)
     
     test_data = []
     test_data =  [df.iloc[:,1].values,df.iloc[:,4].values,df.iloc[:,5].values] #X2 Y2 Y3
     test_data = np.array(test_data)
     test_data= test_data.transpose()
-    test_data = test_data.reshape(1,250,3)
+    test_data = test_data.reshape(1,sLength,3)
     
     test_data_y = df.iloc[:,2].values #X3
     test_data_y = np.array(test_data_y)
-    test_data_y = test_data_y.reshape(1,250) 
+    test_data_y = test_data_y.reshape(1,sLength) 
     input_x = []
     input_x.append(train_data)
     input_x.append(train_data_y)
@@ -87,20 +87,20 @@ def prep_sample(sample,sLength,l):
     train_data = np.array(train_data)
     train_data= train_data.transpose()
     
-    train_data = train_data.reshape(1,250,3)
+    train_data = train_data.reshape(1,sLength,3)
     train_data_y = df.iloc[:,4].values #Y2
     train_data_y = np.array(train_data_y)
-    train_data_y = train_data_y.reshape(1,250)
+    train_data_y = train_data_y.reshape(1,sLength)
     
     test_data = []
     test_data =  [df.iloc[:,4].values,df.iloc[:,1].values,df.iloc[:,3].values] #Y2 X2 X3
     test_data = np.array(test_data)
     test_data= test_data.transpose()
-    test_data = test_data.reshape(1,250,3)
+    test_data = test_data.reshape(1,sLength,3)
     
     test_data_y = df.iloc[:,5].values #Y3
     test_data_y = np.array(test_data_y)
-    test_data_y = test_data_y.reshape(1,250) 
+    test_data_y = test_data_y.reshape(1,sLength) 
     
     input_y = []
     input_y.append(train_data)
@@ -111,43 +111,45 @@ def prep_sample(sample,sLength,l):
     return input_x, input_y
 
     
-def create_model_cnn(train_data_input,train_data_output):
+def create_model_cnn(train_data_input,train_data_output,act,opt,los,ks,sLength):
     model_cnn= Sequential()
-    model_cnn.add(Conv1D(filters=50, kernel_size=100, activation='relu', input_shape=(train_data_input.shape[1],3)))
+    model_cnn.add(Conv1D(filters=50, kernel_size=ks, activation=act, input_shape=(train_data_input.shape[1],3)))
     model_cnn.add(MaxPooling1D(pool_size=100))
     model_cnn.add(Flatten())
-    model_cnn.add(Dense(500, activation='relu'))
-    model_cnn.add(Dense(250))
-    model_cnn.compile(optimizer='adam', loss='mse')
+    model_cnn.add(Dense(2*sLength, activation=act))
+    model_cnn.add(Dense(sLength))
+    model_cnn.compile(optimizer=opt, loss=los)
     history_cnn = model_cnn.fit(train_data_input,train_data_output , epochs=100)
     
     return model_cnn, history_cnn
 
-def create_model_lstm(train_data_input,train_data_output,):
+def create_model_lstm(train_data_input,train_data_output,act,opt,los,u,sLength):
     model_lstm = Sequential()
-    model_lstm.add(LSTM(units=30, return_sequences= True, input_shape=(250,3)))
-    model_lstm.add(LSTM(units=30, return_sequences=True))
-    model_lstm.add(LSTM(units=30))
-    model_lstm.add(Dense(500, activation='relu'))
-    model_lstm.add(Dense(units=250))
+    model_lstm.add(LSTM(units=u, return_sequences= True, input_shape=(sLength,3)))
+    #model_lstm.add(LSTM(units=u, return_sequences=True))
+    model_lstm.add(LSTM(units=u))
+    model_lstm.add(Dense(2*sLength, activation=act))
+    model_lstm.add(Dense(units=sLength))
     model_lstm.summary()
-    model_lstm.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
+    model_lstm.compile(optimizer=opt, loss=los)
     history_lstm = model_lstm.fit(train_data_input, train_data_output, epochs=50, batch_size=32)
     return model_lstm, history_lstm
 
-def create_model_gru(train_data_input,train_data_output,):
+def create_model_gru(train_data_input,train_data_output,act,opt,los,u,sLength):
     model_gru = Sequential()
-    model_gru.add(GRU(units=30, return_sequences= True, input_shape=(train_data_input.shape[1],3)))
-    model_gru.add(GRU(units=30, return_sequences=True))
-    model_gru.add(GRU(units=30))
-    model_gru.add(Dense(500, activation='relu'))
-    model_gru.add(Dense(units=250))
+    model_gru.add(GRU(units=u, return_sequences= True, input_shape=(train_data_input.shape[1],3)))
+    #model_gru.add(GRU(units=u, return_sequences=True))
+    model_gru.add(GRU(units=u))
+    model_gru.add(Dense(2*sLength, activation=act))
+    model_gru.add(Dense(units=sLength))
     model_gru.summary()
-    model_gru.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
+    model_gru.compile(optimizer=opt, loss=los)
     history_gru = model_gru.fit(train_data_input, train_data_output, epochs=50, batch_size=32)
     return model_gru, history_gru
 def show(pred):
     plt.plot(pred)
+
+    
 #show_signal(input_feature)
 #scale_signal(input_feature)
 #sample = pick_sample(t,input_feature)
@@ -180,4 +182,3 @@ def show(pred):
 #plt.plot(prediction_gru)
 #
 #fig = plt.figure()
-
